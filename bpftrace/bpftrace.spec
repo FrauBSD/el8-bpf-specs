6a63898a25ba123ad04092100736fe078806b106
freebsdfrau <freebsdfrau@shxd.cx>
2020-03-02 22:32:19 -0800

    Import initial working set

---
+++ bpftrace/bpftrace.spec
@@ -0,0 +1,189 @@
+%bcond_with static
+%bcond_with git
+
+%global pkgname bpftrace
+
+%define commitid 46e62c0
+
+%if %{with static}
+# The static build is a bit of a hack and
+# doesn't build th docs and tools package
+# so ignore other files
+%global _unpackaged_files_terminate_build 0
+# The post hooks strip the binary which removes
+# the BEGIN_trigger and END_trigger functions
+# which are needed for the BEGIN and END probes
+%global __os_install_post %{nil}
+%global _find_debuginfo_opts -g
+%endif
+
+Name:           %{pkgname}%{?with_static:-static}
+Version:        0.9.3
+%if %{with git}
+Release:        1.%{commitid}%{?dist}
+%else
+Release:        1%{?dist}
+%endif
+Summary:        High-level tracing language for Linux eBPF
+License:        ASL 2.0
+
+URL:            https://github.com/iovisor/bpftrace
+Source0:        %{url}/archive/v%{version}.tar.gz
+Patch0:         0001-build-EL7-support.patch
+Patch1:         0001-tools-ext4dist-based-on-xfsdist.patch
+Patch2:         0001-tools-Patch-for-RHEL7.patch
+
+%if %{with static}
+Patch100:       0001-build-Force-disable-optimization.patch
+Patch101:       0001-Add-lib-iberty-dependency-for-static-builds.patch
+%endif
+
+ExclusiveArch:  x86_64
+
+
+BuildRequires:  bison
+BuildRequires:  flex
+BuildRequires:  cmake3
+BuildRequires:  elfutils-libelf-devel
+BuildRequires:  zlib-devel
+#?BuildRequires:  devtoolset-8-runtime
+#?BuildRequires:  ebpftoolsbuilder-llvm-clang-libs
+
+# For static:
+#?BuildRequires:  bcc-devel
+#?BuildRequires:  bcc-static
+#?BuildRequires:  glibc-static
+#?BuildRequires:  zlib-static
+#?BuildRequires:  ncurses-static
+#?BuildRequires:  elfutils-libelf-devel-static
+#?BuildRequires:  ebpftoolsbuilder-llvm-clang
+#?BuildRequires:  binutils-devel
+
+Requires:       kernel-devel
+Requires:       binutils
+
+Conflicts: bpftrace%{!?with_static:-static}
+
+%description
+BPFtrace is a high-level tracing language for Linux enhanced Berkeley Packet
+Filter (eBPF) available in recent Linux kernels (4.x). BPFtrace uses LLVM as a
+backend to compile scripts to BPF-bytecode and makes use of BCC for
+interacting with the Linux BPF system, as well as existing Linux tracing
+capabilities: kernel dynamic tracing (kprobes), user-level dynamic tracing
+(uprobes), and tracepoints. The BPFtrace language is inspired by awk and C,
+and predecessor tracers such as DTrace and SystemTap
+
+%if !%{with static}
+%package tools
+Summary:        Command line tools for BPFtrace
+BuildArch:      noarch
+
+%description tools
+Command line tools for BPFtrace
+
+%package doc
+Summary:        BPFtrace documentation
+BuildArch:      noarch
+
+%description doc
+BPFtrace documentation
+
+%endif
+
+%prep
+
+%if %{with git}
+rm -rf bpftrace
+git clone %{url} bpftrace
+cd bpftrace
+git checkout %{commitid}
+
+%setup -n bpftrace -D -T
+
+%patch0 -p1
+%patch1 -p1
+%patch2 -p1
+%if %{with static}
+%patch100 -p1
+%patch101 -p1
+%endif
+%else
+%autosetup -p1 -n bpftrace-%{version}
+%endif
+
+%build
+#?. /opt/rh/devtoolset-8/enable
+%cmake3 . \
+  -DCMAKE_BUILD_TYPE=Debug \
+  -DBUILD_TESTING:BOOL=OFF \
+  -DBUILD_SHARED_LIBS:BOOL=OFF \
+%if %{with static}
+  -DSTATIC_LINKING=1
+%endif
+
+%make_build
+
+%install
+%make_install
+
+# Fix shebangs (https://fedoraproject.org/wiki/Packaging:Guidelines#Shebang_lines)
+find %{buildroot}%{_datadir}/%{pkgname}/tools -type f -exec \
+  sed -i -e '1s=^#!/usr/bin/env %{pkgname}\([0-9.]\+\)\?$=#!%{_bindir}/%{pkgname}=' {} \;
+
+# Move man pages to the right location
+mkdir -p %{buildroot}%{_mandir}
+mv %{buildroot}%{_prefix}/man/* %{buildroot}%{_mandir}/
+
+
+%files
+%license LICENSE
+%{_bindir}/%{pkgname}
+
+%if !%{with static}
+%exclude %{_datadir}
+
+%files doc
+%doc README.md CONTRIBUTING-TOOLS.md
+%doc docs/reference_guide.md docs/tutorial_one_liners.md
+
+%files tools
+%dir %{_datadir}/%{pkgname}
+%dir %{_datadir}/%{pkgname}/tools
+%dir %{_datadir}/%{pkgname}/tools/doc
+%{_mandir}/man8/*
+%attr(0755,-,-) %{_datadir}/%{pkgname}/tools/*.bt
+%{_datadir}/%{pkgname}/tools/doc/*.txt
+%endif
+
+
+%changelog
+* Fri Nov 22 2019 bas smit - 0.9.3
+- 0.9.3 release!
+
+* Thu Nov 7 2019 bas smit - 0.9.2
+- Update to latest release
+
+* Sun Jun 30 2019 bas smit - 0.9.1
+- Adept for centos 7
+
+* Thu Apr 25 2019 Augusto Caringi <acaringi@redhat.com> - 0.9-3
+- Rebuilt for bcc 0.9.0
+
+* Mon Apr 22 2019 Neal Gompa <ngompa@datto.com> - 0.9-2
+- Fix Source0 reference
+- Use make_build macro for calling make
+
+* Mon Apr  1 2019 Peter Robinson <pbrobinson@fedoraproject.org> 0.9-1
+- Build on aarch64 and s390x
+
+* Mon Mar 25 2019 Augusto Caringi <acaringi@redhat.com> - 0.9-0
+- Updated to version 0.9
+
+* Thu Jan 31 2019 Fedora Release Engineering <releng@fedoraproject.org> - 0.0-2.20181210gitc49b333
+- Rebuilt for https://fedoraproject.org/wiki/Fedora_30_Mass_Rebuild
+
+* Mon Dec 10 2018 Augusto Caringi <acaringi@redhat.com> - 0.0-1.20181210gitc49b333
+- Updated to latest upstream (c49b333c034a6d29a7ce90f565e27da1061af971)
+
+* Wed Nov 07 2018 Augusto Caringi <acaringi@redhat.com> - 0.0-1.20181107git029717b
+- Initial import
---
+++ build-all.sh
@@ -0,0 +1,363 @@
+#!/bin/sh
+############################################################ IDENT(1)
+#
+# $Title: Script to build bpftrace on CentOS 7.7+ $
+# $Copyright: 2020 Devin Teske. All rights reserved. $
+# $FrauBSD: el8-bpf-specs/build-all.sh 2020-03-02 22:32:19 -0800 freebsdfrau $
+#
+############################################################ ENVIRONMENT
+
+: "${REDHAT:=$( cat /etc/redhat-release )}"
+: "${UNAME_p:=$( uname -p )}"
+: "${UNAME_r:=$( uname -r )}"
+
+############################################################ GLOBALS
+
+pgm="${0##*/}" # Program basename
+
+#
+# Global exit status
+#
+SUCCESS=0
+FAILURE=1
+
+############################################################ FUNCTIONS
+
+exec 3<&1
+eval2(){ printf "\033[32;1m==>\033[m %s\n" "$*" >&3; eval "$@"; }
+have(){ type "$@" > /dev/null 2>&1; }
+quietly(){ "$@" > /dev/null 2>&1; }
+usage(){ die "Usage: %s\n" "$pgm"; }
+
+die()
+{
+	local fmt="$1"
+	if [ "$fmt" ]; then
+		shift 1 # fmt
+		printf "$fmt\n" "$@" >&2
+	fi
+	exit $FAILURE
+}
+
+rpmfilenames()
+{
+	local OPTIND=1 OPTARG flag
+	local exclude=
+	local spec
+	local sp="[[:space:]]*"
+	local dist=
+
+	while getopts x: flag; do
+		case "$flag" in
+		x) exclude="$OPTARG" ;;
+		esac
+	done
+	shift $(( $OPTIND - 1 ))
+
+	spec="$1"
+
+	case "$( cat /etc/redhat-release )" in
+	*" 7."*) dist=.el7 ;;
+	*" 8."*) dist=.el8 ;;
+	esac
+
+	awk -v name_regex="^$sp[Nn][Aa][Mm][Ee]:$sp" \
+	    -v vers_regex="^$sp[Vv][Ee][Rr][Ss][Ii][Oo][Nn]:$sp" \
+	    -v vrel_regex="^$sp[Rr][Ee][Ll][Ee][Aa][Ss][Ee]:$sp" \
+	    -v arch_regex="^$sp[Bb][Uu][Ii][Ll][Dd][Aa][Rr][Cc][Hh]:$sp" \
+	    -v dist="$dist" \
+	    -v default_arch="$( uname -p )" \
+	    -v exclude="$exclude" '
+	################################################## BEGIN
+
+	BEGIN {
+		pkg = 0
+		delete g # globals array
+	}
+
+	################################################## FUNCTIONS
+
+	function show(name,        var, val, left, right)
+	{
+		gsub(/%\{\?dist\}/, dist, name)
+		right = name
+		while (1) {
+			if (!match(right, /%{[^}]+}/)) break
+			var = substr(right, RSTART + 2, RLENGTH - 3)
+			val = var ~ /with_static/ ? "" : g[var]
+			left = left substr(right, 1, RSTART - 1) val
+			right = substr(right, RSTART + RLENGTH)
+		}
+		left = left right
+		if (exclude != "" && left ~ exclude) return
+		print left ".rpm"
+		shown++
+	}
+
+	function dump()
+	{
+		if (pkgname == "") return
+		show(sprintf("%s-%s-%s.%s", pkgname, vers, vrel,
+			pkgarch == "" ? default_arch : pkgarch))
+		pkgname = pkgarch = ""
+	}
+
+	################################################## MAIN
+
+	$1 == "%global" { g[$2] = $3 }
+
+	$1 == "%package" {
+		dump()
+		pkg = 1
+		pkgname = $2 == "-n" ? $3 : name "-" $2
+	}
+
+	sub(name_regex, "") { name = g["name"] = $0 }
+	sub(vers_regex, "") { vers = g["vers"] = $0 }
+	sub(vrel_regex, "") { vrel = g["rel"] = $0 }
+
+	sub(arch_regex, "") {
+		if (pkg) pkgarch = $0; else arch = g["arch"] = $0
+	}
+
+	################################################## END
+
+	END {
+		dump()
+		varch = arch == "" ? default_arch : arch
+		if (name vers vrel != "")
+			show(sprintf("%s-%s-%s.%s", name, vers, vrel, varch))
+		if (shown == 0) exit 1
+		show(sprintf("%s-debuginfo-%s-%s.%s", name, vers, vrel, varch))
+	}
+	' "$spec"
+}
+
+deps()
+{
+	local spec="$1"
+	awk '/^BuildRequires:/{print $2}' "$spec"
+}
+
+build()
+{
+	local OPTIND=1 OPTARG flag
+	local exclude=
+	local tool
+
+	while getopts x: flag; do
+		case "$flag" in
+		x) exclude="$OPTARG" ;;
+		esac
+	done
+	shift $(( $OPTIND - 1 ))
+
+	tool="$1"
+
+	if have figlet; then
+		printf "\033[36m%s\033[m\n" "$( figlet "$tool" )"
+	else
+		printf "\033[36m#\n# Building %s\n#\033[39m\n" "$tool"
+	fi
+
+	local file name
+	local exists=1
+	for file in $( rpmfiles ${exclude:+-x"$exclude"} $tool/$tool.spec ); do
+		name="${file##*/}"
+		if [ -e "$file" ]; then
+			printf "\033[32m%s exists\033[39m\n" "$file"
+			continue
+		fi
+		printf "\033[33m%s does not exist\033[39m\n" "$file"
+		exists=
+	done
+	if [ "$exists" ]; then
+		echo "All RPMS exist (skipping $tool)"
+		return
+	fi
+
+	eval2 cd $tool || die
+
+	eval2 mkdir -p ~/rpmbuild/SOURCES || die
+	for p in *.patch; do
+		[ -e "$p" ] || continue
+		eval2 cp $p ~/rpmbuild/SOURCES/ || die
+	done
+
+	eval2 spectool -g -R $tool.spec || die
+	local needed dep to_install=
+	needed=$( deps $tool.spec ) || die
+	for dep in $needed; do
+		if eval2 quietly rpm -q $dep; then
+			printf "\033[32m%s installed\033[39m\n" "$dep"
+		else
+			printf "\033[33m%s not installed\033[39m\n" "$dep"
+			to_install="$to_install $dep"
+		fi
+	done
+	[ ! "$to_install" ] || eval2 sudo yum install -y $to_install || die
+	( eval2 rpmbuild -bb $tool.spec $*; echo EXIT:$? ) 2>&1 | awk '
+		BEGIN { err = "error: failed to stat .*: "
+			err = err "No such file or directory" }
+		sub(/^EXIT:/, "") { exit_status = $0; next }
+		{ NR_1 = NR_0; print NR_0 = $0 }
+		END { exit NR_1 ~ /^\+ exit 0$/ &&
+			NR_0 ~ "^" err "$" ? 0 : exit_status }
+	' || die
+
+	eval2 cd -
+}
+
+rpmfiles()
+{
+	rpmfilenames "$@" | awk -v p="$HOME/rpmbuild/RPMS/" '
+		BEGIN { sub("/$", "", p) }
+		/-debuginfo-/ { next }
+		{
+			arch = $0
+			sub(/\.rpm$/, "", arch)
+			sub(/.*\./, "", arch)
+			printf "%s/%s/%s\n", p, arch, $0
+		}
+	' # END-QUOTE
+}
+
+yum_install()
+{
+	local need to_install=
+	for need in $*; do
+		if quietly rpm -q $need; then
+			printf "\033[32m%s installed\033[39m\n" "$need"
+		else
+			printf "\033[33m%s not installed\033[39m\n" "$need"
+			to_install="$to_install $need"
+		fi
+	done
+	[ "$to_install" ] || return $SUCCESS
+	eval2 sudo yum install -y $to_install || die
+}
+
+rpm_install()
+{
+	local file to_install=
+	for file in $*; do
+		name=${file##*/}
+		name=${name%%-[0-9]*}
+		rpm -q $name && continue
+		to_install="$to_install $file"
+	done
+	[ "$to_install" ] || return $SUCCESS
+	eval2 sudo rpm -ivh $to_install || die
+}
+
+rpm_uninstall()
+{
+	local path file name installed to_uninstall=
+	for path in $*; do
+		file="${path##*/}"
+		name="${file%%-[0-9]*}"
+		installed=$( rpm -q $name 2> /dev/null ) || continue
+		[ "$installed" != "${file%.rpm}" ] || continue
+		to_uninstall="$to_uninstall $conflict"
+	done
+	if [ "$to_uninstall" ]; then
+		eval2 sudo rpm -e $to_uninstall \|\| : errors ignored
+		local exists=
+		for path in $*; do
+			file="${path##*/}"
+			name="${file%%-[0-9]*}"
+			quietly rpm -q $name || continue
+			exists=1
+			printf "\033[31m%s still installed\033[39m\n" "$path"
+		done
+		[ ! "$exists" ] || die "Uninstall failed"
+	fi
+	return $SUCCESS
+}
+
+############################################################ MAIN
+
+#
+# Process command-line options
+#
+while getopts h flag; do
+	case "$flag" in
+	*) usage # NOTREACHED
+	esac
+done
+shift $(( $OPTIND - 1 ))
+
+#
+# Check system dependencies
+#
+needed="gcc rpmdevtools"
+case "$REDHAT" in
+*" 7."*) [ -e /opt/rh/devtoolset-8/enable ] ||
+	# Tested on 7.7.1908
+	needed="$needed devtoolset-8-runtime"
+	;;
+*" 8."*)
+	# Tested on 8.1.1911
+	needed="$needed gcc-c++"
+	needed="$needed llvm-toolset llvm-devel llvm-static"
+	needed="$needed clang-devel"
+	needed="$needed python3-netaddr"
+	;;
+*)
+	die "Unknown RedHat release ($REDHAT)"
+esac
+needed="$needed kernel-devel-${UNAME_r%.$UNAME_p}"
+eval2 yum_install $needed
+
+#
+# Install bcc build-dependencies
+#
+case "$REDHAT" in
+*" 7."*)
+	build bpftool
+	if ! quietly rpm -q ebpftoolsbuilder-llvm-clang; then
+		build llvm-clang
+		eval2 rpm_uninstall clang clang-devel llvm llvm-devel
+		eval2 rpm_install $( rpmfiles llvm-clang/llvm-clang.spec )
+	fi
+	;;
+*" 8."*)
+	# 8.0: Untested
+	# 8.1: bpftool-4.18.0-147.el8.x86_64 from BaseOS OK
+esac
+
+#
+# Build and install bcc
+# NB: bpftrace dependency
+#
+build -x lua bcc
+files=$( rpmfiles -x lua bcc/bcc.spec ) # with lua = false
+eval2 rpm_uninstall $files # Only uninstalls if version is wrong
+eval2 rpm_install $files
+
+#
+# Install bpftrace build-dependencies
+#
+case "$REDHAT" in
+*" 7."*) needed="ncurses-static binutils-devel" ;;
+*" 8."*) needed="binutils-devel" ;;
+esac
+eval2 yum_install $needed
+
+#
+# Build and install bpftrace
+#
+build bpftrace
+#build bpftrace --with static
+#build bpftrace --with git
+#build bpftrace --with git --with static
+eval2 rpm_install $( rpmfiles bpftrace/bpftrace.spec )
+
+#
+# All software built
+#
+eval2 : SUCCESS
+
+################################################################################
+# END
+################################################################################
